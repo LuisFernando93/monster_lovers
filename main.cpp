e2cee37fd413e70aecb051683345f4b0e1cd2e3f
Luis Fernando Pasquinelli <luispasquinelli@hotmail.com>
2021-10-28 22:12:12 -0300

    Implementacao do controle de fps

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<iostream>
 #include<graphics.h>
 using namespace std;
@@ -11,51 +12,71 @@ int main()  {
 	char Tecla;
 	int pg = 1;
 	int gameState = 1;
+	long long unsigned gt1, gt2;
+  	int fps;
+  	int frame;
+  	
+  	gt1 = GetTickCount();
+  	gt2 = gt1 + 1200;
+  	fps = 30;
+
+	
+	initwindow(WIDTH, HEIGHT);
 	
 	while(Tecla != ESC) {
   		
   		if (gameState == 1) { //jogo
-  		
-			if (pg == 1) pg = 2; else pg = 1;
-	  		setactivepage(pg);
-			cleardevice();
-	  		
-			//update
-			//render
+  			
+  			if (gt2 - gt1 > (1000/fps)) {
+  				
+  				frame = (gt2 - gt1);
+  				printf("%d\n", frame);
+  				gt1 = gt2;
+  				
+  				if (pg == 1) pg = 2; else pg = 1;
+		  		setactivepage(pg);
+				
+				cleardevice();
+				//update
+				//render
+				setvisualpage(pg);
+				
+				frame++;
+			}
 			
-			setvisualpage(pg);
-			
-			fflush(stdin);
-			
-			if (kbhit()) {
-	  			Tecla = getch();
-	  			if(GetKeyState('A')&0x80) { //esquerda
-	  				
+			gt2 = GetTickCount();
+  			
+  			fflush(stdin);
+				
+				if (kbhit()) {
+		  			Tecla = getch();
+		  			if(GetKeyState('A')&0x80) { //esquerda
+		  				
+					} 
+					
+					if(GetKeyState('W')&0x80) { //cima
+		  				
+					}
+					
+					if(GetKeyState('S')&0x80) { //baixo
+		  				
+					} 
+					
+		  			if(GetKeyState('D')&0x80) { //direita
+		  				
+					} 
+					
+		  			if(GetKeyState(VK_SPACE)&0x80) { //jump
+		  				
+					}
+					
+					if(GetKeyState('R')&0x80) { //ataque
+		  				
+					} 
+						
 				} 
-				
-				if(GetKeyState('W')&0x80) { //cima
-	  				
-				}
-				
-				if(GetKeyState('S')&0x80) { //baixo
-	  				
-				} 
-				
-	  			if(GetKeyState('D')&0x80) { //direita
-	  				
-				} 
-				
-	  			if(GetKeyState(VK_SPACE)&0x80) { //jump
-	  				
-				}
-				
-				if(GetKeyState('R')&0x80) { //ataque
-	  				
-				} 
-				
-				
-			} 
-		} else if (gameState == 0) { //game over
+  			
+		} else if (gameState == 0) { //menu
 		
 			fflush(stdin);
 			if (kbhit()) {
